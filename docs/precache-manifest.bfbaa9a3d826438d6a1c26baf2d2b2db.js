754cc3090f2d9755ce17a4d83242b559485dc568
Sean Gransee <sean.gransee@gmail.com>
2019-02-07 19:13:03 -0600

    first commit

---
+++ docs/precache-manifest.bfbaa9a3d826438d6a1c26baf2d2b2db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98a23f152e5553c32085",
+    "url": "/static/css/main.10fac538.chunk.css"
+  },
+  {
+    "revision": "98a23f152e5553c32085",
+    "url": "/static/js/main.98a23f15.chunk.js"
+  },
+  {
+    "revision": "c3636c9d18b7b48eab3c",
+    "url": "/static/js/1.c3636c9d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "074527505a641d70a4a84326b66872ca",
+    "url": "/static/media/background.07452750.jpg"
+  },
+  {
+    "revision": "cdb2f4ba933ced4b20a81e9c301f8cb6",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import moment from "moment"
+
+const renderCountdown = () => {
+  const daysLeft = Math.floor(
+    moment.duration(moment("2019-05-18T20:00:00-0500").diff(moment())).asDays()
+  )
+
+  if (daysLeft >= 1) {
+    return (
+      <div>
+        <div className="count">{daysLeft} days</div>
+        <div>until Shrek's 18th birthday party</div>
+      </div>
+    )
+  } else if (daysLeft === 0) {
+    return (
+      <div>
+        <div className="count">Less than a day</div>
+        <div>until Shrek's 18th birthday party</div>
+      </div>
+    )
+  }
+
+  return <div className="count">IT'S HERE</div>
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="countdown">{renderCountdown()}</div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
